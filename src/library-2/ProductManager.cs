a426bbadddaabd910bab7ea1a807aa5667160c5c
polatengin <enpolat@microsoft.com>
2024-08-30 12:18:40 -0700

    improving productmanager

--- src/library-2/ProductManager.cs
+++ src/library-2/ProductManager.cs
@@ -2,5 +2,13 @@
 
 public class ProductManager
 {
+  public void Add()
+  {
+    Console.WriteLine("Product added!");
+  }
 
+  public void Delete()
+  {
+    Console.WriteLine("Product deleted!");
+  }
 }
